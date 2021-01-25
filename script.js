1bf8d381fab94325aaeea167a76769da1a475d83
bcuser <balbianit@gmail.com>
2021-01-25 14:50:27 -0800

    Fixed js and html

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 let customers = [];
 
-function clearList()
-{
-  let myUl = document.getElementById("list");
-  myUl.innerHTML = "";
-}
-
 document.getElementById("submit").addEventListener("click", function AddCustomer()
 {
-  for (let i = 0; i < 1 ; i++)
-  {
-    customers.push(document.getElementById("custname").value + " " + document.getElementById("city").value + " " + document.getElementById("payment").value);
 
+  let myUL = document.getElementById("list");
+  myUL.innerHTML = "";
+
+  customers.push(document.getElementById("custname").value + " (" + document.getElementById("city").value + ", " + document.getElementById("payment").value + ")");
+  
+  for (let i = 0; i < customers.length ; i++)
+  {
     let ul = document.createElement('ul');
     document.getElementById("list").appendChild(ul);
     let li = document.createElement('li');
     ul.appendChild(li);
     li.innerHTML = customers[i]; 
   };
-})
+
+});
